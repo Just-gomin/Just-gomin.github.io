d9ec08cccb074d116f4d66dd7b8f108f00f9ae15
Kangmin An <hkkm612@gmail.com>
2026-03-12 18:38:33 +0900

    feat: create resume page and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { BLOG_DESCRIPTION, BLOG_TITLE } from "@/lib/constants";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>{BLOG_TITLE}</h1>
       <p>{BLOG_DESCRIPTION}</p>
+      <Link href={"/resume"}>RESUME</Link>
     </div>
   );
 }
---
+++ app/resume/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Resume() {
+  return (
+    <div>
+      <Link href={"/"}>/</Link>
+      <p>This is resume page.</p>
+    </div>
+  );
+}
