bc7f557538a3b28833835d260487c00b92a8e87b
Kangmin An <hkkm612@gmail.com>
2026-03-30 23:25:54 +0900

    feat: show list of posts at `/posts`

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,23 @@
+import { getAllPosts } from "@/features/post";
+import { format } from "date-fns";
 import Link from "next/link";
 
 export default function Posts() {
+  const postList = getAllPosts();
+
   return (
     <div>
       <Link href="/">/</Link>
       <p>This is Posts page.</p>
+      <br />
+      {postList.map((post) => (
+        <Link key={post.slug} href={`/posts/${post.slug}`}>
+          <p>
+            {post.title} | {format(post.date, "yyyy-MM-dd")} |{" "}
+            {post.description}
+          </p>
+        </Link>
+      ))}
     </div>
   );
 }
